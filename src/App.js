5a63dde9360b46c00726fb701d46a7ba243f5b03
Maria Skibniewska <mskibni1@terpmail.umd.edu>
2019-11-16 20:43:07 -0500

    gave menu its own component

--- src/App.js
+++ src/App.js
@@ -2,36 +2,21 @@ import React from 'react';
 import './App.css';
 import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
+import LangMenu from './lang_menu';
 
 function App() {
   const { t } = useTranslation();
 
-  function handleClick(lang) {
-    i18next.changeLanguage(lang)
-  }
-
   return (
     <div className="App">
-        <nav style={{ width: '100%', padding: '2rem 0', backgroundColor:'gray' }}>
-          <button onClick={()=>handleClick('en')} >
-            English
-          </button>
-          <button onClick={()=>handleClick('es')} >
-            Spanish
-          </button>
-          <button onClick={()=>handleClick('cz')} >
-            Czech
-         </button>
-         <button onClick={()=>handleClick('fr')} >
-            French
-         </button>
-        </nav>
+      
         <header className="App-header">
-        
+        <LangMenu />
           <p>
-            <h3>{t("chorus1")}</h3>  
           </p>
         </header>
+        <h3>{t("chorus1")}</h3>  
+
     </div>
   );
 }
--- src/lang_menu.jsx
+++ src/lang_menu.jsx
@@ -3,26 +3,34 @@ import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
 
 export default function LangMenu() {
-  const { t, i18n } = useTranslation();
-  function handleClick(lang) {
-    i18next.changeLanguage(lang)
-  }
-  return (<div>
-<nav>
-<button onClick={()=>handleClick('en')} >
-  English
-</button>
-<button onClick={()=>handleClick('es')} >
-  Spanish
-</button>
-<button onClick={()=>handleClick('fr')} >
-  French
-</button>
-<button onClick={()=>handleClick('cz')} >
-  Czech
-</button>
-</nav>
-  </div>);
+   
+    const { t } = useTranslation();
+
+    function handleClick(language) {
+      i18next.changeLanguage(language);
+    }
+    return (
+<div>
+<nav style={{ width: '100%', padding: '2rem 0', backgroundColor:'gray' }}>
+          <button onClick={()=>handleClick('en')} >
+            English
+          </button>
+          <button onClick={()=>handleClick('es')} >
+            Spanish
+          </button>
+          <button onClick={()=>handleClick('cz')} >
+            Czech
+         </button>
+         <button onClick={()=>handleClick('fr')} >
+            French
+         </button>
+         <button onClick={()=>handleClick('hr')} >
+            Croatian
+         </button>
+        </nav>
+</div>
+    );
+
 };
 
 
