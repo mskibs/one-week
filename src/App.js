72805f0242439d494ea980ad7d01bde050caff7c
maria-skibs <maria.skibniewska@vistaprint.com>
2019-11-20 15:46:10 -0500

    put in a spread

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import React from "react";
 import "./App.css";
 import LangMenu from "./lang_menu";
 import Words from "./words";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCode} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <div className="menu">
-        <LangMenu />
-        </div>
+      <div>
+        <LangMenu className="menu" data-whatever="hi"/>
+      </div>
       <div className="words">
         <Words />
       </div>
 
-    <div className="bottomSection">
-
-<a className="github" href="https://github.com/mskibs/one-week" target="blank"><FontAwesomeIcon icon={faCode} /></a>
-   
-    </div>
-
+      <div className="bottomSection">
+        <a
+          className="github"
+          href="https://github.com/mskibs/one-week"
+          target="blank"
+        >
+          <FontAwesomeIcon icon={faCode} />
+        </a>
+      </div>
     </div>
   );
 }
--- src/lang_menu.jsx
+++ src/lang_menu.jsx
@@ -1,33 +1,19 @@
-import React from 'react';
-import i18next from 'i18next';
+import React from "react";
+import i18next from "i18next";
 
-export default function LangMenu() {
-   
-    function handleClick(language) {
-      i18next.changeLanguage(language);
-    }
-    return (
-<div>
-<nav style={{ width: '100%', padding: '2rem 0' }}>
-          <button onClick={()=>handleClick('en')} >
-            English
-          </button>
-          <button onClick={()=>handleClick('es')} >
-            Spanish
-          </button>
-          <button onClick={()=>handleClick('cs')} >
-            Czech
-         </button>
-         <button onClick={()=>handleClick('fr')} >
-            French
-         </button>
-         <button onClick={()=>handleClick('hr')} >
-            Croatian
-         </button>
-        </nav>
-</div>
-    );
-
-};
-
-
+export default function LangMenu(props) {
+  function handleClick(language) {
+    i18next.changeLanguage(language);
+  }
+  return (
+    <div {...props}>
+      <nav style={{ width: "100%", padding: "2rem 0" }}>
+        <button onClick={() => handleClick("en")}>English</button>
+        <button onClick={() => handleClick("es")}>Spanish</button>
+        <button onClick={() => handleClick("cs")}>Czech</button>
+        <button onClick={() => handleClick("fr")}>French</button>
+        <button onClick={() => handleClick("hr")}>Croatian</button>
+      </nav>
+    </div>
+  );
+}
